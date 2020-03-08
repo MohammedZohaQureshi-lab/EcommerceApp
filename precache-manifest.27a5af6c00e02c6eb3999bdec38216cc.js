0cdcf5d4bddd40bbb52001f4d1327340c6c150ce
Zoha Qureshi <zoha.a.qureshi@avanade.com>
2020-03-08 23:01:01 +0530

    Updates

---
+++ precache-manifest.27a5af6c00e02c6eb3999bdec38216cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4aa676bb2f457b7d33a0d445dc37b9",
+    "url": "/EcommerceApp/index.html"
+  },
+  {
+    "revision": "4b7c9a8d57e28f50e013",
+    "url": "/EcommerceApp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ba9b0ed6a05931ebc9ad",
+    "url": "/EcommerceApp/static/css/main.8b3c573e.chunk.css"
+  },
+  {
+    "revision": "4b7c9a8d57e28f50e013",
+    "url": "/EcommerceApp/static/js/2.55ff25a1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/EcommerceApp/static/js/2.55ff25a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9b0ed6a05931ebc9ad",
+    "url": "/EcommerceApp/static/js/main.4d22ce1b.chunk.js"
+  },
+  {
+    "revision": "d07ac557c0ec21ec3d4d",
+    "url": "/EcommerceApp/static/js/runtime-main.f46bde90.js"
+  }
+]);
